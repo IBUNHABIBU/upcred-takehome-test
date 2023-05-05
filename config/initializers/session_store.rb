c52b7d15796f4ccce51336948596d4a4870e4a64
Ibunhabibu <Ibunhabib@gmail.com>
2023-05-05 17:47:06 +0300

    Add session store

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,5 @@
+if Rails.env == 'production'
+    Rails.application.config.session_store :cookie_store, key: '_interslice_session', domain: 'https://final-capstone-rails-api.herokuapp.com'
+else 
+    Rails.application.config.session_store :cookie_store, key: '_interslice_session'
+end
